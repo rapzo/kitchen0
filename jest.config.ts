e498b21d574f30e94c27b3c8197d03632c9edb05
Rui Lima <ruipedro.lima@gmail.com>
2023-02-10 09:55:40 +0000

    Cleanin up and cpr this one

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+const { getJestProjects } = require('@nrwl/jest');
+
+export default {
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  resolver: '@nrwl/jest/plugins/resolver',
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageReporters: ['html'],
+  projects: getJestProjects(),
+};
